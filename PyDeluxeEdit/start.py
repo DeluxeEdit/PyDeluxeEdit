9a9109f0f6a2cf3d664d5f2be55b592132ee14ed
Pierre Axelsson <pierre.axelsson@outlook.com>
2025-07-30 16:36:35 +0200

    Added textChanged.connect & handleFileChange

--- PyDeluxeEdit/start.py
+++ PyDeluxeEdit/start.py
@@ -1,15 +1,17 @@
+from pydoc import text
+from sqlite3 import connect
 import sys
 from PySide6.QtCore import Slot
 from PySide6.QtWidgets import QApplication, QMainWindow, QTextEdit
 class MainWindow(QMainWindow):
-    @Slot(str)
-    def handleTextChange(self, text: str):
-        print("Button clicked, Hello!")
+    def handleFileChange(self):
+        print("Text changed...>>> ")
 
-    # Create the Qt Application
+    def __init__(self):
+        super().__init__()    
+    
     app = QApplication(sys.argv)
     
-    # Create a button, connect it and show it
     text=QTextEdit()
-    text.changeEvent(handleTextChange)
-# Run the main Qt loop
+    text.textChanged.connect(handleFileChange())
+        
