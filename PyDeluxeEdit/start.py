0592dcc55f33f8037b3815688d68098a92f940e7
Pierre Axelsson <pierre.axelsson@outlook.com>
2025-07-30 20:19:51 +0200

    Added Now use 2 files with proper import

--- PyDeluxeEdit/start.py
+++ PyDeluxeEdit/start.py
@@ -1,8 +1,11 @@
+import sys
+from PySide6.QtWidgets import QApplication
 from window import MainWindow
 class Start():
     def doStart():
+        app = QApplication(sys.argv)
         win=MainWindow()
         win.show()
-
+        app.exec()
 
 
