2d618473982d0d5216a0bc1e1eb87709e047b877
Pierre Axelsson <pierre.axelsson@outlook.com>
2025-07-30 20:33:29 +0200

    Moved app & window handling

--- PyDeluxeEdit/window.py
+++ PyDeluxeEdit/window.py
@@ -1,8 +1,7 @@
 from pydoc import text
 from sqlite3 import connect
-import sys
 from PySide6.QtCore import Slot
-from PySide6.QtWidgets import QApplication, QMainWindow, QTextEdit
+from PySide6.QtWidgets import QMainWindow, QTextEdit
 class MainWindow(QMainWindow):
     def handleFileChange(self):
         print("Text changed...>>> ")
@@ -10,7 +9,6 @@ class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()    
     
-    app = QApplication(sys.argv)
     
     text=QTextEdit()
     text.textChanged.connect(handleFileChange())
