66ea551b41959b2d63a44b0d86775eccf8e74633
Pierre Axelsson <pierre.axelsson@outlook.com>
2025-07-31 18:42:52 +0200

    Mow testing to use QGuiApplication

--- PyDeluxeEdit/start.py
+++ PyDeluxeEdit/start.py
@@ -1,12 +1,13 @@
 import sys
-from PySide6.QtWidgets import QApplication
+from PyQt6.QtWidgets import QGuiApplication 
 from window import MainWindow
-class Start():
-    def _init__(self):
-        app = QApplication(sys.argv)
-        main=MainWindow()
-        main.show()
 
-Start()        
+
+#app = QApplication(sys.argv)
+App = QGuiApplication(sys.argv)
+
+main=MainWindow()
+main.show()
+App.exec()
 
 
