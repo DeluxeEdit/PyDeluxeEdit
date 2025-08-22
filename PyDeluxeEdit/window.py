73fa2b4c7b0fe323e5aa9dda6d2732b429b56035
Pierre Axelsson <pierre.axelsson@outlook.com>
2025-08-22 13:19:05 +0200

    Added setLayout

--- PyDeluxeEdit/window.py
+++ PyDeluxeEdit/window.py
@@ -9,11 +9,10 @@ class App(QMainWindow):
         print("Text changed...>>> ")
     def __init__(self):
         super().__init__()
-        self.layout = QVBoxLayout(self)
-        
+     
+        self.setLayout(QVBoxLayout(self))
 
         self.tabFiles=QTabWidget()
-        self.tabFiles.setVisible(True)
         self.tab=QWidget(self.tabFiles)
         self.tab.text=QTextEdit()
         self.tab.text.textChanged.connect( self.handleFileChange)
@@ -24,8 +23,8 @@ class App(QMainWindow):
         self.tabFiles.addTab(self.tab,"Test")
         self.tabFiles.setTabText(0, "Changed")         
    #     self.setGeometry(10,10, 500, 500)
-        self.layout.a (self.tabFiles)
-        self.layout.addWidget(self.tab.text)
+        
+     
 
         self.setWindowTitle("PyQt6 - Codeloop.org")
         # Set the window icon
