5355b559607f067120b1b2277b2eca783f6756ba
Pierre Axelsson <pierre.axelsson@outlook.com>
2025-08-26 17:37:54 +0200

    Moved text change handler to TextTabItem

--- PyDeluxeEdit/window.py
+++ PyDeluxeEdit/window.py
@@ -4,20 +4,20 @@ import sys
 
 from PyQt6.QtWidgets import QWidget, QTabWidget, QTextEdit, QMainWindow
 class TextTabItem(QWidget):
-    text = QTextEdit()
- 
-class App(QMainWindow):
-  
     def handleFileChange(self, text):
         print("Text changed...>>> ")
 
+    text = QTextEdit()
+    text.textChanged.connect(handleFileChange)
+ 
+class App(QMainWindow):
     def __init__(self):
         super().__init__()
         self.tabFiles=QTabWidget()
    
         self.tab=TextTabItem() 
         #
-        #self.tab.text.textChanged.connect(self.handleFileChange)
+        #self.tab.
         
         self.setCentralWidget(self.tabFiles)
 
@@ -25,9 +25,7 @@ class App(QMainWindow):
         self.tabFiles.setTabText(0, "Changed")
         
 
-        #self.setLayout(self.layout)
-
-   
+      
     
         self.setWindowTitle("PyQt6 - Codeloop.org")
         # Set the window iconf
