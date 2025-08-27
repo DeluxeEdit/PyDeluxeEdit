c9bced03376225cbb1b9c12dbbad4f3b7f957538
Pierre Axelsson <pierre.axelsson@outlook.com>
2025-08-27 15:29:44 +0200

    Found QFormLayout from tutorial & started use it

--- PyDeluxeEdit/window.py
+++ PyDeluxeEdit/window.py
@@ -1,24 +1,30 @@
-from PyQt6.QtWidgets import QWidget, QTabWidget, QTextEdit, QMainWindow, QVBoxLayout
+from PyQt6.QtWidgets import QWidget, QTabWidget, QTextEdit, QMainWindow, QFormLayout
 from PyQt6.QtGui import QIcon
-import sys
-
-from PyQt6.QtWidgets import QWidget, QTabWidget, QTextEdit, QMainWindow
 class TextTabItem(QWidget):
+  
+    def __init__(self):
+        super().__init__()
+        layout = QFormLayout()
+        self.setLayout(layout)
+
+        self.text = QTextEdit()
+        self.text.textChanged.connect(self.handleFileChange)
+        
+        
+        layout.addRow(self.text )
+        self.text.setReadOnly(False)
+        
     def handleFileChange(self, text):
         print("Text changed...>>> ")
 
-    text = QTextEdit()
-    text.textChanged.connect(handleFileChange)
- 
+    
 class App(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.tabFiles=QTabWidget()
-   
-        self.tab=TextTabItem() 
-        #
-        #self.tab.
         
+        self.tabFiles=QTabWidget()
+        self.tab=TextTabItem() 
+   
         self.setCentralWidget(self.tabFiles)
 
         self.tabFiles.addTab(self.tab,"Test")
