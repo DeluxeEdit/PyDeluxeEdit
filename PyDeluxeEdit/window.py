39359b5880ca112139a73e6cd7d5ffb6ee862d77
Pierre Axelsson <pierre.axelsson@outlook.com>
2025-08-27 18:48:02 +0200

    Attempt to solve text change

--- PyDeluxeEdit/window.py
+++ PyDeluxeEdit/window.py
@@ -1,22 +1,20 @@
 from PyQt6.QtWidgets import QWidget, QTabWidget, QTextEdit, QMainWindow, QFormLayout
 from PyQt6.QtGui import QIcon
 class TextTabItem(QWidget):
-  
+    
+    def onTextChanged(str):
+        print("Text changed...>>> ")
+
     def __init__(self):
         super().__init__()
         layout = QFormLayout()
         self.setLayout(layout)
 
-        self.text = QTextEdit()
-        self.text.textChanged.connect(self.handleFileChange)
+        text = QTextEdit()
+        layout.addRow(text)
+        text.textChanged.connect(self.onTextChanged)
         
-        
-        layout.addRow(self.text )
-        self.text.setReadOnly(False)
-        
-    def handleFileChange(self, text):
-        print("Text changed...>>> ")
-
+                
     
 class App(QMainWindow):
     def __init__(self):
