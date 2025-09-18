1ac505ff0303c8ceaec0c7ac79fcac84219406e6
Pierre Axelsson <pierre.axelsson@outlook.com>
2025-09-18 11:26:27 +0200

    Readded imports & & changed event to connect

--- PyDeluxeEdit/window.py
+++ PyDeluxeEdit/window.py
@@ -2,14 +2,16 @@ from fileinput import filename
 from os import path
 from tkinter.filedialog import SaveFileDialog
 from typing import Self
-from PyQt6.QtWidgets import QWidget, QTabWidget, QTextEdit, QMainWindow, QFormLayout,QMenuBar, QFileDialog, QStatusBar,QToolBar, QListWidget
+from PyQt6.QtWidgets import QWidget,QTabWidget, QTextEdit, QMainWindow, QFormLayout, QMenuBar, QFileDialog, QStatusBar, QToolBar,QListWidget
 from PyQt6.QtGui import QIcon, QAction
 from api import Api
+
+
 class TextTabItem(QWidget):
-    
+
     def onTextChanged(text):
         print("Text changed.>>> ")
-   
+
     def __init__(self):
         super().__init__()
         layout = QFormLayout()
@@ -17,31 +19,32 @@ class TextTabItem(QWidget):
         self.text = QTextEdit()
         layout.addRow(self.text)
         self.text.textChanged.connect(self.onTextChanged)
-        
-                
-    
+
+
 class App(QMainWindow):
-   
-    def loadAddFile(path,hexView=False):
-        tab=TextTabItem() 
-        Self.tabFiles.addTab(tab,path)
-        tab.text=Self.api.loadFile(path,hexView)
+
+    def loadAddFile(path, hexView=False):
+        tab = TextTabItem()
+        Self.tabFiles.addTab(tab, path)
+        tab.text = Self.api.loadFile(path, hexView)
         Self.status.showMessage("File:", path)
-                    
+
     def openFileDialog(hexView=False):
         dialog = QFileDialog(Self)
         dialog.setFileMode(QFileDialog.FileMode.ExistingFiles)
         dialog.setNameFilter("Open File", "All files (*.*)")
         dialog.setViewMode(QFileDialog.ViewMode.List)
         if dialog.exec():
-            if dialog.selectedFiles().length ==1:
-                file=dialog.selectedFiles()[0]
-                Self.loadAddFile(path,hexView)
-   
+            if dialog.selectedFiles().length == 1:
+                file = dialog.selectedFiles()[0]
+                Self.loadAddFile(path, hexView)
+
     def saveAsDialog():
-        file = QFileDialog.getSaveFileName(Self, "Save File", "All Files(*);;Text Files(*.txt)")
-        if file:      
-            
+        file = QFileDialog.getSaveFileName(
+            Self, "Save File", "All Files(*);;Text Files(*.txt)"
+        )
+        if file:
+
             def doHexView():
                 Self.openFileDialog(True)
 
@@ -58,30 +61,27 @@ class App(QMainWindow):
         file.addAction(save, Self.saveAsDialog)
         file.addAction(openMenu, Self.openFileDialog)
         file.addAction(hexViewMenu, Self.doHexView)
-            
+
     def statusChanged(text):
         Self.log.addItem(text)
 
-            
     def __init__(self):
-        super().__init__()    
-                    
-        self.status = QStatusBar()
-        self.status.messageChanged(self.statusChanged)
-        self.addPermanentWidget(self.status)
+        super().__init__()
 
-        self.toolbar=QToolBar("Log")
-        self.log=QListWidget(self)
+        self.status = QStatusBar()
+        self.status.messageChanged.connect(self.statusChanged)
+        self.status = QStatusBar()
+        self.status = QStatusBar()
+        self.addPermanentWidget (self.status)
+
+        self.toolbar = QToolBar("Log")
+        self.log = QListWidget(self)
         self.toolbar.addWidget(self.log)
         self.addToolbar(self.toolbar)
-        
-        self.tabFiles=QTabWidget()
+
+        self.tabFiles = QTabWidget()
         self.setCentralWidget(self.tabFiles)
-        self.api=Api()  
+        self.api = Api()
         self.setMenu()
         self.setWindowTitle("PyQt6 - Codeloop.org")
         self.setWindowIcon(QIcon("deluxeedit.png"))
-
-
-
-          
