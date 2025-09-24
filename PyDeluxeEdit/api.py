50d3b377250513cc9df6c393a31450b6c72542a8
Pierre Axelsson <pierre.axelsson@outlook.com>
2025-09-24 22:41:06 +0200

    Simplified loadFile with initial exception

--- PyDeluxeEdit/api.py
+++ PyDeluxeEdit/api.py
@@ -1,10 +1,11 @@
-from ctypes import sizeof
-from genericpath import isfile
+
+from os import path
 import os
-import chardet
+from chardet.universaldetector import UniversalDetector
 from util import * 
 class Api:
     ReadBufferSizeBytes = 32 * 1024
+    
     
     def saveFile(filePath, text):
               with open(filePath, 'w') as f:
@@ -13,15 +14,16 @@ class Api:
 
     def loadFile(filePath,hexView=False):
         result=[]
+        if not path.isfile(filePath): raise FileExistsError(filePath)
+        fileSize=path.getsize(filePath)
+            
         with open(filePath,"r") as myFile:
-            if os.path.isfile(filePath):
-                fileSize=os.path.getsize(filePath)
                 for x in range(0, fileSize /Api.ReadBufferSizeBytes):
                     if hexView:
                         data=myFile.read(Api.ReadBufferSizeBytes)
                         result.append(Util.BytesToHexStrings(data))
                     else:
-                        detector = chardet.universaldetector.UniversalDetector()
+                        detector = UniversalDetector()
                         detector.feed(Api.ReadBufferSizeBytes)
                         lines=detector.done()
                         result.append(lines  )
