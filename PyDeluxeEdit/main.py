f718a7f2f0c67677c245c2727760e55786f726e8
Pierre Axelsson <pierre.axelsson@outlook.com>
2025-09-30 02:22:50 +0200

    Found to use pip packs in vs

--- PyDeluxeEdit/main.py
+++ PyDeluxeEdit/main.py
@@ -1,19 +1,33 @@
 import sys
 #from PySide6.QtCore import Qt
+
 #from PySide6.QtWidgets import QApplication
 
 from PyQt6.QtWidgets import QApplication
-
-#if __name__ == "__main__":
-
-
-app = QApplication(sys.argv)                                                     
-    
-
 from mainWindow import MainWindow
-win=MainWindow()
-win.show()
-app.exec()
+
+def main():
+    app = QApplication(sys.argv)                                                     
+    win=MainWindow()
+    win.show()
+
+    app.exec()
+if __name__ == "__main__":
+    main()
+
+
+
+
+
+
+
+
+
+
+
+
+    89
+   
 
 
  
