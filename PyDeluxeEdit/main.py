d58003c1fa72aaff6cbc1b17704fbf7baff40135
Pierre Axelsson <pierre.axelsson@outlook.com>
2025-10-06 17:39:01 +0200

    Rename variable and testing show

--- PyDeluxeEdit/main.py
+++ PyDeluxeEdit/main.py
@@ -2,16 +2,13 @@ import sys
 from PyQt6.QtWidgets import QApplication
 
 from mainWidget import MainWidget
-
-#if __name__ == "__main__":
-app = QApplication(sys.argv)
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
 
        
 
-win=MainWidget()
-win.show()
-app.exec()
+   
+    mainWidget =MainWidget()
+    mainWidget.show
+    app.exec()
 
-
-
- 
