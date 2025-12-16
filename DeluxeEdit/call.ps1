e2e87ad1e7c56a699147022d948685241c86225e
Pierre Axelsson <pierre.axelsson@outlook.com>
2025-12-16 17:02:45 +0100

    Added SelfSignedCertificate and added  in call

--- DeluxeEdit/call.ps1
+++ DeluxeEdit/call.ps1
@@ -1,1 +1,11 @@
 pyinstaller.exe -n PyDeluxeEdit .\main.py --specpath . -i deluxeedit.ico --clean
+ 
+New-SelfSignedCertificate `
+     -Type Custom `
+     -Subject "CN=Pierre Signing Cert, C=SE" `
+     -KeyUsage DigitalSignature `
+     -FriendlyName "Pierre Cert" `
+     -CertStoreLocation "Cert:\LocalMachine\My" `
+
+#Thumbprint                                Subject
+# 92C6FFA8B964586B45B642528315870AEB22C03B  CN=Pierre Signing Cert, C=SE
