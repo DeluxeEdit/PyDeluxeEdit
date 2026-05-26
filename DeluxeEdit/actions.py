980d3878c90dd67196f3e775eca900c694b879b6
Pierre Axelsson <pierre.axelsson@outlook.com>
2026-05-26 20:48:30 +0200

    Started work with enum

--- DeluxeEdit/actions.py
+++ DeluxeEdit/actions.py
@@ -1,15 +1,19 @@
 from urllib.parse import quote, unquote
+from enum import Enum
+
 class Actions:
-  
-  def run(self, indata,pluginId):return self.ecodeUrl(self,indata)
-  def run(self, indata,pluginId):return self.decodeUrl(self,indata)
-   
-  @staticmethod
-  def ecodeUrl(self,indata):
-    result=quote(indata)
-    return result
+    class ActionId(Enum): URLENCODE = 1; URLDENCODE= 2;
+      
+    def mapEnum(self,indata):
+         map=self.ActionId.URLENCODE 
+
+    def run(self, indata,actionId): return self.ecodeUrl(self,indata)
+    def run(self, indata,actionId):return self.decodeUrl(self,indata)
+
+    def ecodeUrl(self,indata):
+        result=quote(indata)
+        return result
     
-    @staticmethod
     def decodeUrl(self,indata):
         result=unquote(indata)
         return result
