0b3efb1389675dd69e02a8bcc4e06b29568a3637
Pierre Axelsson <pierre.axelsson@outlook.com>
2026-05-27 00:10:49 +0200

    Started mapEnum

--- DeluxeEdit/actions.py
+++ DeluxeEdit/actions.py
@@ -4,8 +4,16 @@ from enum import Enum
 class Actions:
     class ActionId(Enum): URLENCODE = 1; URLDENCODE= 2;
       
-    def mapEnum(self,indata):
-         map=self.ActionId.URLENCODE 
+    
+    def mapEnum(self,indata,actionId):
+        self.mappedFunc=None
+        if actionId is self.ActionId.URLENCODE:
+            mappedFunc=self.ecodeUrl
+        elif actionId is self.ActionId.URLDENCODE:
+            mappedFunc=self.decodeUrl
+          
+        return mappedFunc
+
 
     def run(self, indata,actionId): return self.ecodeUrl(self,indata)
     def run(self, indata,actionId):return self.decodeUrl(self,indata)
