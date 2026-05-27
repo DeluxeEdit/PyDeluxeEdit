2f2d460e1d88097a6cc775ae2535cb3716f053be
Pierre Axelsson <pierre.axelsson@outlook.com>
2026-05-27 13:25:19 +0200

    Added runMapped

--- DeluxeEdit/actions.py
+++ DeluxeEdit/actions.py
@@ -1,24 +1,27 @@
 from urllib.parse import quote, unquote
 from enum import Enum
+from zipfile import ZIP_BZIP2
 
 class Actions:
+    
     class ActionId(Enum): URLENCODE = 1; URLDENCODE= 2;
       
-    
+
+   
     def mapEnum(self,indata,actionId):
         self.mappedFunc=None
         if actionId is self.ActionId.URLENCODE:
-            mappedFunc=self.ecodeUrl
+            self.mappedFunc=self.ecodeUrl
         elif actionId is self.ActionId.URLDENCODE:
-            mappedFunc=self.decodeUrl
+            self.mappedFunc=self.decodeUrl
           
-        return mappedFunc
+        return self.mappedFunc
 
-
-    def run(self, indata,actionId): return self.ecodeUrl(self,indata)
-    def run(self, indata,actionId):return self.decodeUrl(self,indata)
-
-    def ecodeUrl(self,indata):
+    def runMapped(self,indata,actionId):
+      mapped=  self.mapEnum(indata,actionId)
+      if (mapped): return mapped(indata)
+     
+      def ecodeUrl(self,indata):
         result=quote(indata)
         return result
     
